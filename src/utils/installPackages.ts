c9c5115e46f467a519426d8325f8ac34f313b983
BayBreezy <behon.baker@yahoo.com>
2024-01-29 08:21:31 -0500

    fix: check for postinstall script before running it

--- src/utils/installPackages.ts
+++ src/utils/installPackages.ts
@@ -1,6 +1,27 @@
+import * as fs from "fs";
+import * as path from "path";
 import { execa } from "execa";
 import _ from "lodash";
 import ora from "ora";
+
+function checkPackageJson() {
+  const packageJsonPath = path.join(process.cwd(), "package.json");
+  let scriptExists = false;
+
+  // Check if package.json file exists
+  if (fs.existsSync(packageJsonPath)) {
+    // Read the package.json file
+    const packageJsonContent = fs.readFileSync(packageJsonPath, "utf-8");
+    const packageJson = JSON.parse(packageJsonContent);
+
+    // Check if "postinstall" script is defined
+    const postinstallScript = packageJson.scripts?.postinstall;
+    if (postinstallScript) {
+      scriptExists = true;
+    }
+  }
+  return scriptExists;
+}
 
 export const installPackages = async (
   packageManager: string,
@@ -22,7 +43,13 @@ export const installPackages = async (
   if (!_.isUndefined(devDeps) && !_.isEmpty(devDeps)) {
     await execa(packageManager, [packageManager === "yarn" ? "add" : "install", "-D", ...devDeps]);
   }
-  await execa(packageManager, ["run", "postinstall"]);
+
+  // Check if package.json file exists
+  if (checkPackageJson()) {
+    // we should check to see if there is a postinstall script and run it
+    depsSpinner.text = "Running postinstall script...";
+    await execa(packageManager, ["run", "postinstall"]);
+  }
 
   depsSpinner.succeed("Installed dependencies!");
 };
