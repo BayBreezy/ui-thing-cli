507db3d4b0e8c2f0b16ac1be7118d1807cf05f25
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2024-08-31 12:03:55 -0500

    chore: use new version of nuxt icon

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,7 @@ export const INIT_DEPS = ["radix-vue", "tailwind-variants"];
 export const INIT_DEV_DEPS = [
   "typescript",
   "tailwindcss-animate",
-  "nuxt-icon",
+  "@nuxt/icon",
   "prettier-plugin-tailwindcss",
   "prettier",
   "@nuxtjs/tailwindcss",
@@ -16,7 +16,7 @@ export const INIT_MODULES = [
   "@nuxtjs/tailwindcss",
   "@nuxtjs/color-mode",
   "@vueuse/nuxt",
-  "nuxt-icon",
+  "@nuxt/icon",
 ];
 
 export const CSS_THEME_OPTIONS = [
