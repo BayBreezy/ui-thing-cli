d3a316759f50c47b1bf29257242098121f949c72
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-08-13 16:50:57 -0400

    refactor(installPackages): remove package.json check and streamline postinstall execution

--- src/utils/installPackages.ts
+++ src/utils/installPackages.ts
@@ -1,27 +1,6 @@
-import * as fs from "fs";
-import * as path from "path";
 import { execa } from "execa";
 import _ from "lodash";
 import ora from "ora";
-
-function checkPackageJson() {
-  const packageJsonPath = path.join(process.cwd(), "package.json");
-  let scriptExists = false;
-
-  // Check if package.json file exists
-  if (fs.existsSync(packageJsonPath)) {
-    // Read the package.json file
-    const packageJsonContent = fs.readFileSync(packageJsonPath, "utf-8");
-    const packageJson = JSON.parse(packageJsonContent);
-
-    // Check if "postinstall" script is defined
-    const postinstallScript = packageJson.scripts?.postinstall;
-    if (postinstallScript) {
-      scriptExists = true;
-    }
-  }
-  return scriptExists;
-}
 
 export const installPackages = async (
   packageManager: string,
@@ -44,12 +23,9 @@ export const installPackages = async (
     await execa(packageManager, [packageManager === "yarn" ? "add" : "install", "-D", ...devDeps]);
   }
 
-  // Check if package.json file exists
-  if (checkPackageJson()) {
-    // we should check to see if there is a postinstall script and run it
-    depsSpinner.text = "Running postinstall script...";
-    await execa(packageManager, ["run", "postinstall"]);
-  }
+  // we should check to see if there is a postinstall script and run it
+  depsSpinner.text = "Running nuxt prepare...";
+  await execa`npx -y nuxt prepare`;
 
   depsSpinner.succeed("Installed dependencies!");
 };
