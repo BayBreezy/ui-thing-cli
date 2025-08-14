e140d92c3d93c82657936b0405980fa55412f163
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-08-14 01:54:51 -0400

    refactor(constants): update UI configuration and dependencies for Nuxt 3 and 4

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,41 +1,95 @@
+import { UIConfig } from "../types";
+
+/**
+ * The filename of the UI Thing configuration file.
+ */
+export const UI_CONFIG_FILENAME = "ui-thing.config.ts";
+/**
+ * The default UI Thing configuration.
+ *
+ * Used when Nuxt 3 is detected
+ */
+export const DEFAULT_CONFIG: UIConfig = {
+  theme: "zinc",
+  tailwindCSSLocation: "assets/css/tailwind.css",
+  componentsLocation: "components/Ui",
+  composablesLocation: "composables",
+  pluginsLocation: "plugins",
+  utilsLocation: "utils",
+  force: true,
+  useDefaultFilename: true,
+  packageManager: "npm",
+};
+
+/**
+ * The default UI Thing configuration.
+ *
+ * Used when Nuxt 4 is detected
+ */
+export const DEFAULT_CONFIG_NUXT4: UIConfig = {
+  theme: "zinc",
+  tailwindCSSLocation: "app/assets/css/tailwind.css",
+  componentsLocation: "app/components/Ui",
+  composablesLocation: "app/composables",
+  pluginsLocation: "app/plugins",
+  utilsLocation: "app/utils",
+  force: true,
+  useDefaultFilename: true,
+  packageManager: "npm",
+};
+
+/**
+ * The initial core dependencies
+ */
 export const INIT_DEPS = [
-  "radix-vue",
+  "tailwindcss",
+  "motion-v",
+  "@tailwindcss/vite",
+  "reka-ui",
   "tailwind-variants",
+  "tailwind-merge",
   "@nuxt/fonts",
-  "@nuxtjs/tailwindcss",
   "@nuxtjs/color-mode",
   "@nuxt/icon",
   "@vueuse/nuxt",
   "@tailwindcss/forms",
-  "tailwindcss-animate",
 ];
+
+/**
+ * The initial development dependencies
+ */
 export const INIT_DEV_DEPS = [
   "typescript",
   "prettier-plugin-tailwindcss",
   "prettier",
   "@ianvs/prettier-plugin-sort-imports",
+  "tw-animate-css",
 ];
 
+/**
+ * Initial modules that are needed
+ */
 export const INIT_MODULES = [
-  "@nuxtjs/tailwindcss",
   "@nuxtjs/color-mode",
+  "motion-v/nuxt",
   "@vueuse/nuxt",
   "@nuxt/icon",
   "@nuxt/fonts",
 ];
 
-export const NUXT_VERSIONS = [
-  { title: "Nuxt 3", value: 3 },
-  { title: "Nuxt 4", value: 4 },
-];
-
+/**
+ * List of available package managers to chose from
+ */
 export const PACKAGE_MANAGER_CHOICES = [
-  { title: "NPM", value: "npm" },
-  { title: "YARN", value: "yarn" },
-  { title: "PNPM", value: "pnpm" },
-  { title: "BUN", value: "bun" },
+  { title: "Npm", value: "npm" },
+  { title: "Yarn", value: "yarn" },
+  { title: "Pnpm", value: "pnpm" },
+  { title: "Bun", value: "bun" },
 ];
 
+/**
+ * List of available CSS themes to choose from
+ */
 export const CSS_THEME_OPTIONS = [
   { title: "Zinc", value: "zinc" },
   { title: "Slate", value: "slate" },
