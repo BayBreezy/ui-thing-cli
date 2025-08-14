69b36adb6ff830e933ab7500750c0c62ae881bb2
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-08-14 01:58:52 -0400

    refactor(promptForComponents): fetch components dynamically for user selection

--- src/utils/promptForComponents.ts
+++ src/utils/promptForComponents.ts
@@ -1,9 +1,13 @@
 import prompts from "prompts";
 
-import allComponents from "../comps";
 import { Component } from "../types";
+import { fetchComponents } from "./fetchComponents";
 
+/**
+ * Prompts the user to select components to add.
+ */
 export const promptUserForComponents = async (all?: boolean): Promise<string[]> => {
+  const allComponents = await fetchComponents();
   // If all is true, return all components
   if (all) return allComponents.map((c: Component) => c.value);
   const { components } = await prompts({
