aa77801784636912a65d39224859439c09ccee49
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-12-31 06:31:11 -0500

    feat: enhance promptUserForComponents to support ProseComponent type

--- src/utils/promptForComponents.ts
+++ src/utils/promptForComponents.ts
@@ -1,21 +1,24 @@
 import prompts from "prompts";
 
-import { Component } from "../types";
+import { Component, ProseComponent } from "../types";
 
 /**
  * Prompts the user to select components to add.
  */
 export const promptUserForComponents = async (
   all?: boolean,
-  allComponents: Component[] = []
+  allComponents: Component[] | ProseComponent[] = []
 ): Promise<string[]> => {
   // If all is true, return all components
-  if (all) return allComponents.map((c: Component) => c.value);
+  if (all) return allComponents.map((c: Component | ProseComponent) => c.value);
   const { components } = await prompts({
     type: "autocompleteMultiselect",
     name: "components",
     message: "Select the components you want to add",
-    choices: allComponents.map((c: Component) => ({ title: c.name, value: c.value })),
+    choices: allComponents.map((c: Component | ProseComponent) => ({
+      title: c.name,
+      value: c.value,
+    })),
   });
   return components;
 };
